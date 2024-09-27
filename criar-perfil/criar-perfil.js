926df5ef8ac18beceab61813d0bf34f3977f0b8c
geovannarc <geovannarcf@gmail.com>
2024-09-27 17:55:15 -0300

    primeiro commit

---
+++ criar-perfil/criar-perfil.js
@@ -0,0 +1,14 @@
+const fileInput = document.getElementById('file-input');
+const profilePic = document.getElementById('profile-pic');
+
+fileInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            profilePic.src = e.target.result;  // Mostra a imagem na interface
+        }
+        reader.readAsDataURL(file);
+        
+    }
+});
