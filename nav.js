bc0bebf3d4c66cfa223878a96ff5a923364b77ba
geovannarc <geovannarcf@gmail.com>
2024-11-21 01:24:48 -0300

    fix links nav

--- nav.js
+++ nav.js
@@ -4,25 +4,25 @@ function createNav() {
     nav.innerHTML = `<nav class="navbar navbar-light">
         <ul class="navbar-nav d-flex flex-row">
          <li class="nav-item">
-          <a class="nav-link" href="https://geovannarc.github.io/foodies/destaques">
+          <a class="nav-link" href="https://foodiesapp.com.br/destaques">
            <i class="fas fa-home">
            </i>
           </a>
          </li>
          <li class="nav-item">
-          <a class="nav-link" href="https://geovannarc.github.io/foodies/pesquisar">
+          <a class="nav-link" href="https://foodiesapp.com.br/pesquisar">
            <i class="fas fa-search">
            </i>
           </a>
          </li>
          <li class="nav-item">
-          <a class="nav-link" href="https://geovannarc.github.io/foodies/criar-post/busca-restaurante">
+          <a class="nav-link" href="https://foodiesapp.com.br/criar-post/busca-restaurante">
            <i class="fas fa-plus-square">
            </i>
           </a>
          </li>
          <li class="nav-item">
-          <a class="nav-link" href="https://geovannarc.github.io/foodies/perfil/meu-perfil">
+          <a class="nav-link" href="https://foodiesapp.com.br/perfil/meu-perfil">
            <i class="fas fa-user">
            </i>
           </a>
